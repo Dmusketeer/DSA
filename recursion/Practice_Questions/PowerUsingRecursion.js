2aa6b5d0d578a4dfe6635b0e7ff750e640ed68eb
Dmusketeer <ardmountronics@gmail.com>
2021-08-06 20:49:08 +0530

    calculating power using the recursion

--- recursion/Practice_Questions/PowerUsingRecursion.js
+++ recursion/Practice_Questions/PowerUsingRecursion.js
@@ -8,3 +8,15 @@ function pow(m,n){
     }
 }
 console.log(pow(4, 3));
+
+
+function pow(m, n) {
+    if (n === 0) {
+        return 1;
+    } if(n%2===0) {
+        return pow(m*m, n/2);
+    }else{
+        return m*pow(m * m, (n-1 )/ 2);
+    }
+}
+console.log(pow(4, 3));

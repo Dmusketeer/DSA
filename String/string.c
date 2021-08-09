cbbe732cc5295427877debe199cfe2feaed57ad2
Dmusketeer <ardmountronics@gmail.com>
2021-08-09 16:23:49 +0530

    count the number of vowels and consonents

--- String/string.c
+++ String/string.c
@@ -1,20 +1,20 @@
 // Length of string
-// #include <stdio.h>
-// #include <string.h>
-//
-// int main() {
-//   char a[100];
-//   int length;
+#include <stdio.h>
+#include <string.h>
 
-//   printf("Enter a string to calculate its length\n");
-//   gets(a);
+int main() {
+  char a[100];
+  int length;
 
-//   length = strlen(a);
+  printf("Enter a string to calculate its length\n");
+  gets(a);
 
-//   printf("Length of the string = %d\n", length);
+  length = strlen(a);
 
-//   return 0;
-// }
+  printf("Length of the string = %d\n", length);
+
+  return 0;
+}
 
 
 
@@ -38,5 +38,37 @@ int main(){
         c++;
     }
     printf("%s\n", s);
+    return 0;
+}
+
+
+
+
+// Count Number Of Vowels &Consonants In A String
+#include <stdio.h>
+#include <string.h>
+
+    int
+    main()
+{
+    char s[1000];
+    int i, vowels = 0, consonants = 0;
+
+    printf("Enter  the string : ");
+    gets(s);
+    //   store the string into the variable ‘s’
+    for (i = 0; s[i]; i++)
+    {
+        if ((s[i] >= 65 && s[i] <= 90) || (s[i] >= 97 && s[i] <= 122))
+        {
+
+            if (s[i] == 'a' || s[i] == 'e' || s[i] == 'i' || s[i] == 'o' || s[i] == 'u' || s[i] == 'A' || s[i] == 'E' || s[i] == 'I' || s[i] == 'O' || s[i] == 'U')
+                vowels++;
+            else
+                consonants++;
+        }
+    }
+    printf("vowels = %d\n", vowels);
+    printf("consonants = %d\n", consonants);
     return 0;
 }

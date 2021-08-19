6a1a65987266424260ff7878d3ec91fe8175798e
Dmusketeer <ardmountronics@gmail.com>
2021-08-19 21:34:37 +0530

    Array is sorted or not

---
+++ Array/Array_sorted.cpp
@@ -0,0 +1,34 @@
+// check array is sorted or not
+#include<iostream>
+
+using namespace std;
+
+struct Array{
+    int A[10];
+    int size;
+    int length;
+};
+
+void display(struct Array arr){
+    for(int i=0; i<arr.length-1; i++){
+        cout<<arr.A[i]<<" ";
+    }
+}
+
+int isSorted(struct Array arr){
+    for(int i=0; i<arr.length-1; i++) {
+    if(arr.A[i]>arr.A[i+1]){
+        return 0;
+        break;
+    }
+    }
+    return 1;
+}
+
+int main(){
+
+    struct Array arr1 = {{1,2,3,4,5},5,5};
+    cout << isSorted(arr1)<<endl;
+    display(arr1);
+    return 0;
+}

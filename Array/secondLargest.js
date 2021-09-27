64e48547aa642d963bcfcb17eef808145e9e8c44
Dmusketeer <ardmountronics@gmail.com>
2021-09-27 09:30:21 +0530

    secondlargestnumber in array

---
+++ Array/secondLargest.js
@@ -0,0 +1,10 @@
+function getSecondLargest(nums) {
+    // Complete the function
+    let results = nums.sort((a, b) => a - b)
+        .filter((el, index, array) => index === array.indexOf(el));
+
+    return results[results.length - 2];
+}
+
+
+console.log(getSecondLargest([23, 1, 5, 7, 9, 10, 23]))

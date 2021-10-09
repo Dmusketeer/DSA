f889baf071ba0ab9ad20341ec171137ba9bc991f
Dmusketeer <ardmountronics@gmail.com>
2021-10-09 11:26:35 +0530

    upper trangular matrix

---
+++ Matrices/upperTranngularMatrix.js
@@ -0,0 +1,15 @@
+function Lower(matrix, rows, cols) {
+    for (let i = 0; i < rows; i++) {
+        for (let j = 0; j < cols; j++) {
+            if (i > j) console.log('0');
+            else console.log(`${matrix[i][j]}`);
+        }
+        console.log('\t')
+    }
+    console.log('\n')
+}
+let matrix = [[1, 2, 3],
+[4, 5, 6],
+[7, 8, 9]];
+let row = 3, col = 3;
+Lower(matrix, row, col)

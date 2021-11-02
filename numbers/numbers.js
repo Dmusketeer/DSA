4b72201c544add4ca04c890b36ecd58c2b4e12e1
Dmusketeer <ardmountronics@gmail.com>
2021-11-02 19:35:19 +0530

    maximums and minimums

--- numbers/numbers.js
+++ numbers/numbers.js
@@ -28,3 +28,22 @@ console.log("Math.ceil :", Math.ceil(5.3));
 console.log("Math.ceil :", Math.ceil(0.5));
 console.log("Math.ceil :", Math.ceil(1.8));
 
+
+// Number.EPSILON
+function numberEqual(x, y) {
+    return Math.abs(x - y) < Number.EPSILON
+}
+
+console.log(numberEqual(0.4, 0.2));
+console.log(numberEqual(0.2, 0.2));
+
+
+// Number.MAX_SAFE_INTEGER [returns the largest integer]
+// Number.MIN_SAFE_INTEGER [returns the smallest integer]
+// Number.MIN_VALUE [returns the smallest floating-point number possible.]
+// Number.MAX_VALUE [ returns the largest floating-point number possible.]
+
+console.log("Largest integer : ", Number.MAX_SAFE_INTEGER);
+console.log("Smallest integer : ", Number.MIN_SAFE_INTEGER);
+console.log("Min floating point value : ", Number.MIN_VALUE);
+console.log("Max floating point value : ", Number.MAX_VALUE);

49359f017ae080ae579d32acea42812250df8d57
Dmusketeer <ardmountronics@gmail.com>
2021-11-10 21:58:25 +0530

    array

---
+++ Array/partOf450Que/sort012.js
@@ -0,0 +1,11 @@
+// Given an array of size N containing only 0s, 1s, and 2s; sort the array in ascending order.
+
+
+function sort012(arr, size) {
+    max = arr[0]
+    for (var i = 1; i < arr.length; i++) {
+        if (arr[i] > max) {
+            max = arr[i]
+        })
+    }
+}
